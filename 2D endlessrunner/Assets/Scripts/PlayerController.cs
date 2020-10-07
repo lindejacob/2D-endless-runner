a61d1516ad9b45c79fa756bf744002c0ab32e7f9
lindejacob <lindejacob0612@gmail.com>
2020-10-07 17:41:15 +0200

    Started on playercontroller

---
+++ 2D endlessrunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(xAxis, 0);
+
+        rb2d.AddForce(movement * speed);
+    }
+}
